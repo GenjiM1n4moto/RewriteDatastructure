80573e9c444ba6c3136d18d7563a2026e7ceb42f
Pengju He <hepj1999@gmail.com>
2023-01-25 21:36:50 +0900

    Write basic node and test
    insert, deletebyIndex, output finished and tested

---
+++ node.go
@@ -0,0 +1,82 @@
+package rewritedatastructure
+
+import "errors"
+
+type Node struct {
+	val  interface{}
+	next *Node
+}
+
+func NewNode(val interface{}) *Node {
+	return &Node{
+		val:  val,
+		next: nil,
+	}
+}
+
+// Node.insert(val interface{}) insert the val in the tail of the node
+func (n *Node) insert(val interface{}) error {
+	if n.val == nil && n.next == nil {
+		return errors.New("Empty node")
+	}
+	// Find tail
+	temp := n
+	for temp.next != nil {
+		temp = temp.next
+	}
+	// Insert
+	temp.next = NewNode(val)
+	return nil
+}
+
+// Node.output() return the values of the node in order
+func (n *Node) output() ([]interface{}, error) {
+	if n.val == nil && n.next == nil {
+		return nil, errors.New("Empty node")
+	}
+	temp := n
+	logs := []interface{}{}
+	for temp != nil {
+		logs = append(logs, temp.val)
+		temp = temp.next
+	}
+	return logs, nil
+}
+
+// deleteIndex delete the indexth position of the node. For example, if index is 0, delete the first element of the node
+func (n *Node) deleteIndex(index int) (*Node, error) {
+	// empty node
+	if n.val == nil && n.next == nil {
+		return nil, errors.New("empty node")
+	}
+	if n.next == nil {
+		return nil, errors.New("Too short nodelist to be deleted")
+	}
+	temp := n
+	i := 0
+	for i < index {
+		if temp.next != nil {
+			i += 1
+			temp = temp.next
+		} else {
+			return nil, errors.New("The index is larger than the lenght of nodelist")
+		}
+	}
+	// head
+	if temp == n {
+		return n.next, nil
+	}
+	// tail and ordinary
+	new_temp := n
+	// find the node before temp
+	for new_temp.next != temp {
+		new_temp = new_temp.next
+	}
+	if temp.next != nil {
+		temp_next := temp.next
+		new_temp.next = temp_next
+	} else {
+		new_temp.next = nil
+	}
+	return n, nil
+}
---
+++ node_test.go
@@ -0,0 +1,128 @@
+package rewritedatastructure
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestOutput(t *testing.T) {
+	testNodeList1 := &Node{
+		val: 1,
+		next: &Node{
+			val:  "test",
+			next: nil,
+		},
+	}
+	testNodeList2 := &Node{}
+	testNodeList1Array, err := testNodeList1.output()
+	require.NoError(t, err)
+	require.Equal(t, []interface{}{1, "test"}, testNodeList1Array)
+	testNodeList2Array, err := testNodeList2.output()
+	require.Nil(t, testNodeList2Array)
+	require.Error(t, err)
+}
+
+func TestInsert(t *testing.T) {
+	testNodeList1 := &Node{
+		val: 1,
+		next: &Node{
+			val:  "test",
+			next: nil,
+		},
+	}
+	err := testNodeList1.insert("test2")
+	require.NoError(t, err)
+	testNodeList1Array, err := testNodeList1.output()
+	require.NoError(t, err)
+	require.Equal(t, testNodeList1Array, []interface{}{1, "test", "test2"})
+
+	testNodeList2 := &Node{}
+	err = testNodeList2.insert(1)
+	require.Error(t, err)
+}
+
+func TestDeleteIndex(t *testing.T) {
+	testNodeList := &Node{
+		val:  1,
+		next: nil,
+	}
+	_, err := testNodeList.deleteIndex(1)
+	require.Error(t, err)
+	_, err = testNodeList.deleteIndex(0)
+	require.Error(t, err)
+
+	testNodeList = &Node{
+		val: 1,
+		next: &Node{
+			val: "test",
+			next: &Node{
+				val:  "test2",
+				next: nil,
+			},
+		},
+	}
+	head, err := testNodeList.deleteIndex(1)
+	require.NoError(t, err)
+	outputhead, err := head.output()
+	require.NoError(t, err)
+	require.Equal(t, []interface{}{1, "test2"}, outputhead)
+
+	testNodeList = &Node{
+		val: 1,
+		next: &Node{
+			val: "test",
+			next: &Node{
+				val: "test2",
+				next: &Node{
+					val:  "test3",
+					next: nil,
+				},
+			},
+		},
+	}
+	head, err = testNodeList.deleteIndex(3)
+	require.NoError(t, err)
+	outputhead, err = head.output()
+	require.NoError(t, err)
+	require.Equal(t, []interface{}{1, "test", "test2"}, outputhead)
+
+	testNodeList = &Node{}
+	_, err = testNodeList.deleteIndex(0)
+	require.Error(t, err)
+
+	testNodeList = &Node{
+		val: 1,
+		next: &Node{
+			val: "test",
+			next: &Node{
+				val: "test2",
+				next: &Node{
+					val:  "test3",
+					next: nil,
+				},
+			},
+		},
+	}
+	head, err = testNodeList.deleteIndex(0)
+	require.NoError(t, err)
+	outputhead, err = head.output()
+	require.NoError(t, err)
+	require.Equal(t, []interface{}{"test", "test2", "test3"}, outputhead)
+
+	testNodeList = &Node{
+		val: 1,
+		next: &Node{
+			val: "test",
+			next: &Node{
+				val: "test2",
+				next: &Node{
+					val:  "test3",
+					next: nil,
+				},
+			},
+		},
+	}
+	_, err = testNodeList.deleteIndex(10)
+	require.Error(t, err)
+}
